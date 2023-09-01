695937396b4acd588b4dfefa4a6fd349bdb7e7e2
savage <2443005004@qq.com>
2023-09-02 04:38:05 +0800

    docs: try

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 import apidocConfig from '../apidocConfig.json'
 
 export default defineConfig({
-	// base: '',
+	base: '/npm-electron-ipc/',
 	title: 'savage-electron-ipc',
 	themeConfig: {
 		sidebar: {
